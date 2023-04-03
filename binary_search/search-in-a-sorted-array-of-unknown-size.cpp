9a68f4d6a99e1bdaae756b459bdd280bf2ca09a5
mubashar-sundarstem <129356604+mubashar-sundarstem@users.noreply.github.com>
2023-04-03 16:02:50 +0500

    Update search-in-a-sorted-array-of-unknown-size.cpp

--- binary_search/search-in-a-sorted-array-of-unknown-size.cpp
+++ binary_search/search-in-a-sorted-array-of-unknown-size.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int search(const ArrayReader& reader, int target) {
-        int pl = 0;
-        int pr = pow(10,4);
-        int mid = (pr+pl)/2;
-        while(pl<=pr){
-            mid = (pr+pl)/2;
-            cout << mid << ' ';
-            if(reader.get(mid)==target){
-                return mid;
+    int shipWithinDays(vector<int>& weight, int days) {
+        int weight_sz = weight.size();
+    int Asum = 0;
+    int maxe = 0;
+    for(int i = 0;i < weight_sz;i++){
+        int h = weight[i];
+        maxe = max(maxe,h);
+        Asum+=h;
+    }Asum+=1;
+    int leftp = maxe;//left pointer
+    int rightp = Asum;//right pointer
+    int sumcheck = 0;
+    int daycheck = 0;
+    int ans = INT_MAX;
+    int mid = (leftp+rightp)/2;
+    while(leftp <= rightp){
+        mid = (leftp+rightp)/2;
+        sumcheck = 0;
+        daycheck = 0;
+        for(int i = 0;i < weight_sz;i++){
+            if(sumcheck+weight[i] > mid){
+                daycheck++;
+                sumcheck = 0;
             }
-            if(reader.get(mid) > target){
-                pr = mid-1;
-            }else{
-                pl = mid+1;
-            }
-        }cout << endl;
-        if(reader.get(mid)==target)
-            return mid;
-        else
-            return -1;
+            sumcheck+=weight[i];
+        }if(sumcheck > 0){
+            daycheck++;
+        }
+        if(daycheck > days){
+            leftp = mid+1;
+        }else{
+            ans = min(ans,mid);
+            rightp = mid-1;
+        }
+    }return ans;
     }
 };
